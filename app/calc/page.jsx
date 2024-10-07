6ef190bbaa9890285d622a0502dc363da4980837
Meelenych <meelenych@gmail.com>
2024-10-07 17:14:22 -0600

    formulas fix, content fix

--- app/calc/page.jsx
+++ app/calc/page.jsx
@@ -11,8 +11,8 @@ const CalcPage = () => {
 	};
 
 	const [formData, setFormData] = useState(initialFormData);
-	const [loJack, setLoJack] = useState(0);
-	const [roadHazard, setRoadHazard] = useState(0);
+	const [loJack, setLoJack] = useState(499);
+	const [roadHazard, setRoadHazard] = useState(499);
 
 	const handleLoJackChange = e => {
 		setLoJack(e.target.checked ? 499 : 0);
@@ -30,9 +30,11 @@ const CalcPage = () => {
 		Number(formData.otherAccessory) -
 		equity;
 
-	const totalWithDown = total - Number(formData.downPayment);
-	const totalWithTenDown = total - Number(total * 1.1 * 0.1);
-	const totalWithTwentyDown = total - Number(total * 1.1 * 0.2);
+	const totalWithTax = total * 1.12;
+	const totalWithDown = totalWithTax - Number(formData.downPayment);
+	const totalWithTenDown = totalWithTax - Number(totalWithTax * 0.1);
+	const totalWithTwentyDown = totalWithTax - Number(totalWithTax * 0.2);
+	const interestRate = 1.1;
 
 	const toCurrency = n => {
 		return n.toLocaleString('en-US', {
@@ -47,14 +49,15 @@ const CalcPage = () => {
 	};
 
 	return (
-		<div className='flex justify-evenly items-center container mx-auto gap-5 p-5 flex-col sm:flex-row'>
+		<div className='min-h-screen flex flex-col sm:flex-row justify-evenly items-center gap-5 p-5'>
 			{/* Form inputs */}
 			<div className='w-full md:w-1/4 flex flex-col items-center justify-center'>
-				<h2>Enter the values here</h2>
-				<form className='grid grid-cols-1 gap-4 w-full'>
-					<label htmlFor='trade'>
-						{' '}
-						Trade value
+				<h2>Enter the following data here</h2>
+				<form className='grid grid-cols-1 w-full gap-2 min-w-0.5'>
+					<label
+						htmlFor='trade'
+						className='text-xs text-center flex flex-col'>
+						<span>Trade value</span>
 						<input
 							className='w-full rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
 							type='number'
@@ -65,25 +68,35 @@ const CalcPage = () => {
 							onChange={handleInputChange}
 						/>
 					</label>
-					<input
-						className='rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
-						type='number'
-						name='payoff'
-						id='payoff'
-						placeholder='Trade payoff'
-						value={formData.payoff}
-						onChange={handleInputChange}
-					/>
-					<input
-						className='rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
-						type='number'
-						name='price'
-						id='price'
-						placeholder='Vehicle price'
-						value={formData.price}
-						onChange={handleInputChange}
-					/>
-					<div className='rounded-full border border-solid border-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 flex items-center'>
+					<label
+						htmlFor='payoff'
+						className='text-xs text-center flex flex-col'>
+						<span>Trade payoff</span>
+						<input
+							className='rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
+							type='number'
+							name='payoff'
+							id='payoff'
+							placeholder='Trade payoff'
+							value={formData.payoff}
+							onChange={handleInputChange}
+						/>
+					</label>
+					<label
+						htmlFor='price'
+						className='text-xs text-center flex flex-col'>
+						<span>Vehicle price</span>
+						<input
+							className='rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
+							type='number'
+							name='price'
+							id='price'
+							placeholder='Vehicle price'
+							value={formData.price}
+							onChange={handleInputChange}
+						/>
+					</label>
+					<div className='mt-4 rounded-full border border-solid border-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 flex items-center'>
 						<div className='form-control w-full'>
 							<label className='label cursor-pointer flex justify-between'>
 								<span className='label-text'>LoJack</span>
@@ -96,7 +109,7 @@ const CalcPage = () => {
 							</label>
 						</div>
 					</div>
-					<div className='rounded-full border border-solid border-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 flex items-center'>
+					<div className='mt-4 rounded-full border border-solid border-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 flex items-center'>
 						<div className='form-control w-full'>
 							<label className='label cursor-pointer'>
 								<span className='label-text'>Road Hazard</span>
@@ -109,27 +122,37 @@ const CalcPage = () => {
 							</label>
 						</div>
 					</div>
-					<input
-						className='rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
-						type='number'
-						name='otherAccessory'
-						id='otherAccessory'
-						placeholder='Additional accessories cost'
-						value={formData.otherAccessory}
-						onChange={handleInputChange}
-					/>
-					<input
-						className='rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
-						type='number'
-						name='downPayment'
-						id='downPayment'
-						placeholder='Down payment'
-						value={formData.downPayment}
-						onChange={handleInputChange}
-					/>
+					<label
+						htmlFor='otherAccessory'
+						className='text-xs text-center flex flex-col'>
+						<span>Other accessories cost</span>
+						<input
+							className='rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
+							type='number'
+							name='otherAccessory'
+							id='otherAccessory'
+							placeholder='Additional accessories cost'
+							value={formData.otherAccessory}
+							onChange={handleInputChange}
+						/>
+					</label>
+					<label
+						htmlFor='downPayment'
+						className='text-xs text-center flex flex-col'>
+						<span>Initial cash investment</span>
+						<input
+							className='rounded-full border border-solid border-transparent transition-colors  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
+							type='number'
+							name='downPayment'
+							id='downPayment'
+							placeholder='Down payment'
+							value={formData.downPayment}
+							onChange={handleInputChange}
+						/>
+					</label>
 				</form>
 				{/* Interest rates */}
-				<div className='dropdown dropdown-hover dropdown-top w-full mt-4'>
+				<div className='dropdown dropdown-hover dropdown-top w-full mt-6'>
 					<div
 						tabIndex={0}
 						role='button'
@@ -139,16 +162,16 @@ const CalcPage = () => {
 
 					<div
 						tabIndex={0}
-						className='dropdown-content menu bg-base-100 rounded-box z-[1] w-max p-2 shadow'>
+						className='dropdown-content menu bg-base-100 rounded-box z-[1] w-full p-2 shadow'>
 						<h5 className='text-lg'>Credit-based Rates</h5>
-						<table className='table'>
+						<table className='table table-xs'>
 							{/* head */}
 							<thead>
 								<tr>
-									<th>CREDIT GRADE</th>
-									<th>FICO SCORE</th>
-									<th>UP TO 60 MONTHS</th>
-									<th>61-72 MONTHS</th>
+									<th>Grade</th>
+									<th>Score</th>
+									<th>Up to 60 mths </th>
+									<th>61-72 mths</th>
 								</tr>
 							</thead>
 							<tbody>
@@ -209,22 +232,19 @@ const CalcPage = () => {
 							</p>
 						</li>
 						<li>
-							<p>
-								Total price with accessories including trade cost and equity:{' '}
-								{toCurrency(total)}
-							</p>
+							<p>Total with accessories, trade cost and equity: {toCurrency(total)}</p>
 						</li>
 						<li>
 							<p>
-								Total + 10% for Tax, Title, Licensing and Registration:{' '}
-								{toCurrency(total * 1.1)}
+								Total + Tax, Title, Licensing and Registration:{' '}
+								{toCurrency(totalWithTax)}
 							</p>
 						</li>
 						<li>
-							<p>20% down payment: {toCurrency(total * 1.1 * 0.2)}</p>
+							<p>20% down payment: {toCurrency(totalWithTax * 0.2)}</p>
 						</li>
 						<li>
-							<p>10% down payment: {toCurrency(total * 1.1 * 0.1)}</p>
+							<p>10% down payment: {toCurrency(totalWithTax * 0.1)}</p>
 						</li>
 					</ul>
 				</div>
@@ -239,19 +259,32 @@ const CalcPage = () => {
 						/>
 
 						<div className='collapse-title text-xl font-medium'>
-							<h3>Payments without money down:</h3>
+							<h3>Without money down:</h3>
 						</div>
 						<div className='collapse-content'>
-							<h4>Short term</h4>
+							<h3 className='text-lg'>
+								Amount to be financed: {toCurrency(totalWithTax)}
+							</h3>
+							<h4>Short term payments for:</h4>
 							<ul>
-								<li>Monthly payment 24: {toCurrency(((total * 1.1) / 24) * 1.05)}</li>
-								<li>Monthly payment 36: {toCurrency(((total * 1.1) / 36) * 1.05)}</li>
+								<li>
+									24 months: {toCurrency(((totalWithTax * interestRate) / 24) * 1.05)}
+								</li>
+								<li>
+									36 months: {toCurrency(((totalWithTax * interestRate) / 36) * 1.05)}
+								</li>
 							</ul>
-							<h4>Long term</h4>
+							<h4>Long term payments for:</h4>
 							<ul>
-								<li>Monthly payment 60: {toCurrency(((total * 1.1) / 60) * 1.05)}</li>
-								<li>Monthly payment 72: {toCurrency(((total * 1.1) / 72) * 1.05)}</li>
-								<li>Monthly payment 84: {toCurrency(((total * 1.1) / 84) * 1.05)}</li>
+								<li>
+									60 months: {toCurrency(((totalWithTax * interestRate) / 60) * 1.05)}
+								</li>
+								<li>
+									72 months: {toCurrency(((totalWithTax * interestRate) / 72) * 1.05)}
+								</li>
+								<li>
+									84 months: {toCurrency(((totalWithTax * interestRate) / 84) * 1.05)}
+								</li>
 							</ul>
 						</div>
 					</div>
@@ -262,19 +295,32 @@ const CalcPage = () => {
 							name='my-accordion-4'
 						/>
 						<div className='collapse-title text-xl font-medium'>
-							<h3>Payments with custom money down:</h3>
+							<h3>With custom money down:</h3>
 						</div>
 						<div className='collapse-content'>
-							<h4>Short term</h4>
+							<h3 className='text-lg'>
+								Amount to be financed: {toCurrency(totalWithDown)}
+							</h3>
+							<h4>Short term payments for:</h4>
 							<ul>
-								<li>Monthly payment 24: {toCurrency((totalWithDown / 24) * 1.05)}</li>
-								<li>Monthly payment 36: {toCurrency((totalWithDown / 36) * 1.05)}</li>
+								<li>
+									24 months: {toCurrency(((totalWithDown * interestRate) / 24) * 1.05)}
+								</li>
+								<li>
+									36 months: {toCurrency(((totalWithDown * interestRate) / 36) * 1.05)}
+								</li>
 							</ul>
-							<h4>Long term</h4>
+							<h4>Long term payments for:</h4>
 							<ul>
-								<li>Monthly payment 60: {toCurrency((totalWithDown / 60) * 1.05)}</li>
-								<li>Monthly payment 72: {toCurrency((totalWithDown / 72) * 1.05)}</li>
-								<li>Monthly payment 84: {toCurrency((totalWithDown / 84) * 1.05)}</li>
+								<li>
+									60 months: {toCurrency(((totalWithDown * interestRate) / 60) * 1.05)}
+								</li>
+								<li>
+									72 months: {toCurrency(((totalWithDown * interestRate) / 72) * 1.05)}
+								</li>
+								<li>
+									84 months: {toCurrency(((totalWithDown * interestRate) / 84) * 1.05)}
+								</li>
 							</ul>
 						</div>
 					</div>
@@ -285,28 +331,36 @@ const CalcPage = () => {
 							name='my-accordion-4'
 						/>
 						<div className='collapse-title text-xl font-medium'>
-							<h3>Payments with 10% money down:</h3>
+							<h3>With 10% money down:</h3>
 						</div>
 						<div className='collapse-content'>
-							<h4>Short term</h4>
+							<h3 className='text-lg'>
+								Amount to be financed: {toCurrency(totalWithTenDown)}
+							</h3>
+							<h4>Short term payments for:</h4>
 							<ul>
 								<li>
-									Monthly payment 24: {toCurrency((totalWithTenDown / 24) * 1.05)}
+									24 months:{' '}
+									{toCurrency(((totalWithTenDown * interestRate) / 24) * 1.05)}
 								</li>
 								<li>
-									Monthly payment 36: {toCurrency((totalWithTenDown / 36) * 1.05)}
+									36 months:{' '}
+									{toCurrency(((totalWithTenDown * interestRate) / 36) * 1.05)}
 								</li>
 							</ul>
-							<h4>Long term</h4>
+							<h4>Long term payments for:</h4>
 							<ul>
 								<li>
-									Monthly payment 60: {toCurrency((totalWithTenDown / 60) * 1.05)}{' '}
+									60 months:{' '}
+									{toCurrency(((totalWithTenDown * interestRate) / 60) * 1.05)}
 								</li>
 								<li>
-									Monthly payment 72: {toCurrency((totalWithTenDown / 72) * 1.05)}
+									72 months:{' '}
+									{toCurrency(((totalWithTenDown * interestRate) / 72) * 1.05)}
 								</li>
 								<li>
-									Monthly payment 84: {toCurrency((totalWithTenDown / 84) * 1.05)}
+									84 months:{' '}
+									{toCurrency(((totalWithTenDown * interestRate) / 84) * 1.05)}
 								</li>
 							</ul>
 						</div>
@@ -318,28 +372,36 @@ const CalcPage = () => {
 							name='my-accordion-4'
 						/>
 						<div className='collapse-title text-xl font-medium'>
-							<h3>Payments with 20% money down:</h3>
+							<h3>With 20% money down:</h3>
 						</div>
 						<div className='collapse-content'>
-							<h4>Short term</h4>
+							<h3 className='text-lg'>
+								Amount to be financed: {toCurrency(totalWithTwentyDown)}
+							</h3>
+							<h4>Short term payments for:</h4>
 							<ul>
 								<li>
-									Monthly payment 24: {toCurrency((totalWithTwentyDown / 24) * 1.05)}
+									24 months:{' '}
+									{toCurrency(((totalWithTwentyDown * interestRate) / 24) * 1.05)}
 								</li>
 								<li>
-									Monthly payment 36: {toCurrency((totalWithTwentyDown / 36) * 1.05)}
+									36 months:{' '}
+									{toCurrency(((totalWithTwentyDown * interestRate) / 36) * 1.05)}
 								</li>
 							</ul>
-							<h4>Long term</h4>
+							<h4>Long term payments for:</h4>
 							<ul>
 								<li>
-									Monthly payment 60: {toCurrency((totalWithTwentyDown / 60) * 1.05)}{' '}
+									60 months:{' '}
+									{toCurrency(((totalWithTwentyDown * interestRate) / 60) * 1.05)}{' '}
 								</li>
 								<li>
-									Monthly payment 72: {toCurrency((totalWithTwentyDown / 72) * 1.05)}
+									72 months:{' '}
+									{toCurrency(((totalWithTwentyDown * interestRate) / 72) * 1.05)}
 								</li>
 								<li>
-									Monthly payment 84: {toCurrency((totalWithTwentyDown / 84) * 1.05)}
+									84 months:{' '}
+									{toCurrency(((totalWithTwentyDown * interestRate) / 84) * 1.05)}
 								</li>
 							</ul>
 						</div>
