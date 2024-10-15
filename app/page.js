257f868b55b04b83143c6ebd816557b4a6ede960
Meelenych <meelenych@gmail.com>
2024-10-15 13:57:09 -0600

    target blank removed from links

--- app/page.js
+++ app/page.js
@@ -22,7 +22,6 @@ export default function Home() {
 					<a
 						className='rounded-lg border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
 						href='/calc'
-						target='_blank'
 						rel='noopener noreferrer'>
 						<Image
 							className='dark'
@@ -108,7 +107,6 @@ export default function Home() {
 					<a
 						href='/tips'
 						className='rounded-lg border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center gap-2 hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
-						target='_blank'
 						rel='noopener noreferrer'>
 						<Image
 							className='dark:invert'
